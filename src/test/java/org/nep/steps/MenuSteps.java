b5a5005b5b54b2a8766114129cb9a146c4297e55
Oleg Vinogradov <freiend_of_step@mail.ru>
2023-03-15 11:49:43 +0300

    Add wait before

--- src/test/java/org/nep/steps/MenuSteps.java
+++ src/test/java/org/nep/steps/MenuSteps.java
@@ -1,14 +1,17 @@
 package org.nep.steps;
 
+import com.codeborne.selenide.Condition;
 import io.cucumber.java.ru.Пусть;
 import org.nep.pages.MenuPage;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.page;
 
 public class MenuSteps {
     @Пусть("^переходим меню в Еосаго$")
     public void eosagoTab()  {
         MenuPage menuPage = page(MenuPage.class);
+        menuPage.eosagoTab.shouldBe(visible);
         menuPage.eosagoTab.click();
 
     }
