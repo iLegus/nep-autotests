82f6e7d20b6b14131b43a1c2d30d08f7751d3903
korusant <vektorcom@yandex.ru>
2023-10-24 16:54:17 +0300

    добил антиклещ

--- src/test/java/org/nep/steps/AntikleshSteps.java
+++ src/test/java/org/nep/steps/AntikleshSteps.java
@@ -2,7 +2,11 @@ package org.nep.steps;
 
 import io.cucumber.java.ru.Пусть;
 import org.nep.pages.AntikleshPage;
+
+import java.time.Duration;
 import java.util.Map;
+
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.page;
 import static org.nep.Utils.DateUtil.SIMPLE_DOT;
 import static org.nep.Utils.DateUtil.getDate;
@@ -11,6 +15,19 @@ public class AntikleshSteps {
     public void AntikleshData(Map<String, String> values){
         AntikleshPage antikleshPage = page (AntikleshPage.class);
         AntikleshPage.BirthDate.setValue(values.get("ДатаРождения"));
+        AntikleshPage.BtnCalc.click();
+        AntikleshPage.BtnSave.shouldBe(visible, Duration.ofSeconds(50)).click();
+        AntikleshPage.FioInsurer.setValue(values.get("ФИОСтрахователя")).pressEnter();
+        AntikleshPage.BirthDate.setValue(values.get("ДатаРождения"));
+        AntikleshPage.Pasp.setValue(values.get("Паспорт"));
+        AntikleshPage.PaspPlace.setValue(values.get("Место"));
+        AntikleshPage.Phone.setValue(values.get("Телефон"));
+        AntikleshPage.Email.setValue(values.get("Почта"));
+        AntikleshPage.FioInsured.setValue(values.get("ФИОСтрахуемого")).pressEnter();
+        AntikleshPage.Citizenship.click();
+        AntikleshPage.Country.click();
+
+        AntikleshPage.Kid.click();
     }
 
 }
