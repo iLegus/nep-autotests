f4e7cf2541f2c3c33e30505713837c8ee6588e76
korusant <vektorcom@yandex.ru>
2023-10-25 14:38:15 +0300

    клещ до ссылки

--- src/main/java/org/nep/pages/AntikleshPage.java
+++ src/main/java/org/nep/pages/AntikleshPage.java
@@ -13,6 +13,7 @@ public class AntikleshPage {
     public static SelenideElement AkBirthDateSave = $(By.id("insured_birth_date"));
     public static SelenideElement AkPasp = $(By.id("insured_series_and_number"));
     public static SelenideElement AkPaspPlace = $(By.id("insured_place_out"));
+    public static SelenideElement AkPaspDateOut = $(By.id("insured_date_out"));
     public static SelenideElement AkPhone = $(By.id("insured_phone1"));
     public static SelenideElement AkEmail = $(By.id("insured_email"));
     public static SelenideElement AkFioInsured = $(By.id("Insured1__FIO"));
@@ -20,4 +21,5 @@ public class AntikleshPage {
     public static SelenideElement AkBuy = $(By.xpath("//button[@class='nep-button nep-button_green-50 nep-button_icon-left nep-button_full-width nep-button_white-space-normal nep-button_lg']"));
     public static SelenideElement AkCountry = $(By.xpath("//ul[@class='ps-container nep-scrollbar nep-scrollbar_absolute nep-select__options nep-select__options_null ps ps--active-y']/li[1]"));
     public static SelenideElement AkCitizenship = $(By.id("insured1_citizenship"));
+    public static SelenideElement AkPaySum = $(By.xpath("//div[@class='pay-link-modal-data__item-value']"));
 }
--- src/test/java/org/nep/steps/AntikleshSteps.java
+++ src/test/java/org/nep/steps/AntikleshSteps.java
@@ -21,13 +21,15 @@ public class AntikleshSteps {
         AntikleshPage.AkBirthDateSave.setValue(values.get("ДатаРождения"));
         AntikleshPage.AkPasp.setValue(values.get("Паспорт"));
         AntikleshPage.AkPaspPlace.setValue(values.get("Место"));
+        AntikleshPage.AkPaspDateOut.setValue(values.get("ДатаВыдачи"));
         AntikleshPage.AkPhone.setValue(values.get("Телефон"));
         AntikleshPage.AkEmail.setValue(values.get("Почта"));
         AntikleshPage.AkFioInsured.setValue(values.get("ФИОСтрахуемого")).pressEnter();
         AntikleshPage.AkCitizenship.click();
         AntikleshPage.AkCountry.click();
-
         AntikleshPage.AkKid.click();
+        AntikleshPage.AkBuy.click();
+        AntikleshPage.AkPaySum.shouldBe(visible, Duration.ofSeconds(60)).isDisplayed();
     }
 
 }
